7906f9d7713c179b89d41402ec3f31d2a282517d
Trần Quang Huy <84978747+tqhb2502@users.noreply.github.com>
2025-11-26 17:58:35 +0700

    Update script to use relative path for process.py

--- tsllm/offline_rl/rlhf/process.sh
+++ tsllm/offline_rl/rlhf/process.sh
@@ -3,4 +3,4 @@ INPUT_DIR="./rlhf/cot_sample"
 OUTPUT_DIR="./rlhf/processed"
 mkdir -p $OUTPUT_DIR
 
-python3 process.py --input_dir=$INPUT_DIR --output_dir=$OUTPUT_DIR
+python3 rlhf/process.py --input_dir=$INPUT_DIR --output_dir=$OUTPUT_DIR
