8070413981cfdfc826d512464d9b6d572e07473c
Trần Quang Huy <84978747+tqhb2502@users.noreply.github.com>
2025-11-26 18:03:34 +0700

    Update model paths and reduce processes in script

--- train_mcts_scripts/rlhf/test_policy_and_value.sh
+++ train_mcts_scripts/rlhf/test_policy_and_value.sh
@@ -6,9 +6,9 @@ export TEST_WITH_TERMINAL=1
 
 # export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-CT2_DIR={your ct2 model cache}
-CRITIC_PATH={your critic model cache}
-torchrun --nproc_per_node=8 --master-port 29503 ../../tsllm/offline_rl/test_sft_and_v_rlhf.py \
+CT2_DIR=/content/LLM_Tree_Search/ct2_cache/gpt2
+CRITIC_PATH=/content/LLM_Tree_Search/value_models/gpt2-open-instruct-v1/last_model_hf
+torchrun --nproc_per_node=1 --master-port 29503 ../../tsllm/offline_rl/test_sft_and_v_rlhf.py \
     --critic_model_path $CRITIC_PATH \
     --tokenizer_path $CRITIC_PATH \
     --ct2_dir $CT2_DIR \
