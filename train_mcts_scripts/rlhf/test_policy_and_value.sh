fee04329764f0b456751caa70cdd052480c56db1
Trần Quang Huy <84978747+tqhb2502@users.noreply.github.com>
2025-11-27 01:33:24 +0700

    Update paths for RLHF test script

--- train_mcts_scripts/rlhf/test_policy_and_value.sh
+++ train_mcts_scripts/rlhf/test_policy_and_value.sh
@@ -6,9 +6,9 @@ export TEST_WITH_TERMINAL=1
 
 # export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-CT2_DIR=/content/LLM_Tree_Search/ct2_cache/gpt2
-CRITIC_PATH=/content/LLM_Tree_Search/value_models/gpt2-open-instruct-v1/last_model_hf
-torchrun --nproc_per_node=1 --master-port 29503 ../../tsllm/offline_rl/test_sft_and_v_rlhf.py \
+CT2_DIR=/content/LLM_Tree_Search_RLHF_Test/ct2_cache/gpt2
+CRITIC_PATH=/content/LLM_Tree_Search_RLHF_Test/value_models/gpt2-open-instruct-v1/last_model_hf
+torchrun --nproc_per_node=1 --master-port 29503 /content/LLM_Tree_Search_RLHF_Test/tsllm/offline_rl/test_sft_and_v_rlhf.py \
     --critic_model_path $CRITIC_PATH \
     --tokenizer_path $CRITIC_PATH \
     --ct2_dir $CT2_DIR \
